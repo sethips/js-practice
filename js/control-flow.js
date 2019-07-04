51ca72b49275fa6568a129750896157219ef1f37
AbrahamDN <40493309+AbrahamDN@users.noreply.github.com>
2019-07-04 20:34:32 +0100

    Control-flow
    Control-flow lesson complete

--- js/control-flow.js
+++ js/control-flow.js
@@ -142,16 +142,150 @@
 //Every +5 from speed limit = +1 point
 // 12 points -> Suspended
 
-function checkSpeed(speed) {
+// function checkSpeed(speed) {
 
-    const speedLimit = 70;
-    speed = Math.floor(speed);
-    let points = (speed - speedLimit) / 5;
+//     const speedLimit = 70;
+//     speed = Math.floor(speed);
+//     const points = (speed - speedLimit) / 5;
 
-    return (points >= 12) ?
-        'Suspended' : 'Valid ' + (12 - points) + ' points remain';
+//     return (points >= 12) ?
+//         'Suspended' : 'Valid ' + (12 - points) + ' points remain';
 
-}
+// }
 
 
-console.log(checkSpeed(300));
+// console.log(checkSpeed(80));
+
+
+// EXERCISE 5 - Even and Odd Numbers
+
+// function showNumbers(num) {
+//     for (let count = 0; count <= num; count++) {
+//         console.log(count, (count % 2 === 0) ? 'EVEN' : 'ODD');
+//     }
+// }
+
+// showNumbers(10);
+
+// EXERCISE 6 - Count Truthy
+
+// const array = [undefined, 0, null, '', 1, 2, 3];
+
+// function countTruthy(array) {
+//     let count = 0;
+//     for (const value of array) {
+//         if (value)
+//             count++;
+//     }
+//     return count;
+// }
+
+// console.log(countTruthy(array));
+
+
+// EXERCISE 7- String Properties
+// const movie = {
+//     title: 'movieTitle',
+//     releaseYear: 2018,
+//     rating: 4.5,
+//     director: 'directorName'
+// }
+
+// for (const key in movie) {
+//     if (typeof movie[key] === 'string')
+//         console.log(key, movie[key]);
+// }
+
+
+// EXERCISE 8- Sum of Multiples of 3 and 5
+
+// function sum(limit) {
+//     let sum = 0;
+//     for (let i = 0; i < limit; i++) {
+//         if (i % 3 === 0 || i % 5 === 0)
+//             sum += i;
+//     }
+//     return sum;
+// }
+
+// console.log(sum(10));
+
+
+// EXERCISE 9 - Grade
+// 1-59: F
+// 60-69: D
+// 70-79: C
+// 80-89: B
+// 90-100: A
+
+// const marks = [100, 110, 60];
+
+// function calcAverage(array) {
+//     let sum = 0;
+
+//     for (let value of array)
+//         sum += value;
+
+//     return sum / array.length;
+// }
+
+// function gradeCalc(marks) {
+//     const average = calcAverage(marks);
+
+//     if (average <= 59) return 'F';
+//     if (average <= 69) return 'D';
+//     if (average <= 79) return 'C';
+//     if (average <= 89) return 'B';
+//     if (average <= 100) return 'A';
+
+//     return average;
+// }
+
+// console.log(gradeCalc(marks));
+
+
+// EXERCISE 10 - Stars
+// function showStars(rows) {
+//     for (let row = 0; row < rows; row++) {
+//         let pattern = '';
+//         for (let i = 0; i < row; i++)
+//             pattern += '*';
+//         console.log(pattern);
+//     }
+// }
+
+// showStars(10);
+
+
+// EXERCISE 11 - Prime Numbers
+
+//My faulty solution
+// for (let i = 0; i <= 10; i++) {
+//     let zero = [];
+//     let h;
+//     for (let y = 2; y <= 20; y++) {
+//         // console.log(i % y);
+//         let calc = i % y;
+//         if (calc === 0)
+//             zero.push(calc);
+//         if (zero.length <= 1)
+//             h = calc;
+//     }
+
+//     console.log(h);
+// }
+
+//Good solution
+// function calcPrimes(num) {
+//     for (let factor = 2; factor < num; factor++)
+//         if (num % factor === 0)
+//             return false;
+//     return true;
+// }
+
+// function showPrimes(limit) {
+//     for (let num = 2; num <= limit; num++)
+//         if (calcPrimes(num)) console.log(num);
+// }
+
+// showPrimes(10);
